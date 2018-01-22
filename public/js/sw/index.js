90406776d9e48695ef1de48084b3ab4fdce00165
hhassan1230 <hhassan@oswego.edu>
2018-01-21 22:53:32 -0500

    adds working new response to sw

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,10 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  event.respondWith(
+  	new Response (
+  		"<div class='a-winner-is-me'>Ah Lemme ask you something!</div>", {
+  		headers: {"Content-Type": "text/html"}
+  	})
+  );
 });
