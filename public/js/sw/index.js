f87e04e4e4ce0ba3067d98f151962c2f122ae391
hhassan1230 <hhassan@oswego.edu>
2018-01-22 22:34:51 -0500

    adds sw cached

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,20 +1,33 @@
+var urlsToCache = [
+  '/',
+  'js/main.js',
+  'css/main.css',
+  'imgs/icon.png',
+  'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+  'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+];
 self.addEventListener('install', function(event) {
-  var urlsToCache = [
-    '/',
-    'js/main.js',
-    'css/main.css',
-    'imgs/icon.png',
-    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-  ];
+
 
   event.waitUntil(
-    // TODO: open a cache named 'wittr-static-v1'
-    // Add cache the urls from urlsToCache
+    caches.open('wittr-static-v1').then(function(cache){
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
+    })
   );
 });
 
 self.addEventListener('fetch', function(event) {
-  // Leave this blank for now.
-  // We'll get to this in the next task.
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
 });
