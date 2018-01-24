935bd6dd0db7dc4918e810b70b95c3aacf254bfd
hhassan1230 <hhassan@oswego.edu>
2018-01-23 19:53:05 -0500

    understanding force reload

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -26,7 +26,9 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      if (response) return response;
+      if (response) {
+        return response;
+      }
       return fetch(event.request);
     })
   );
